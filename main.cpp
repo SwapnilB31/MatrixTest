8413cf80f7e29215b1b49de7c83a6255c66cfc6c
Swapnil Bhattacharjee <swapnilbhattacharjee187@gmail.com>
2018-09-08 21:04:51 +0530

    redundant code deleted

--- main.cpp
+++ main.cpp
@@ -174,40 +174,7 @@ void lcm::calculate(void)
 		while(true);
 	}
 	
-	
-	
-	void adjoint(int A[N][N],int adj[N][N]) 
-{ 
-    if (N == 1) 
-    { 
-        adj[0][0] = 1; 
-        return; 
-    } 
-  
-    // temp is used to store cofactors of A[][] 
-    int sign = 1, temp[N][N]; 
-  
-    for (int i=0; i<N; i++) 
-    { 
-        for (int j=0; j<N; j++) 
-        { 
-            // Get cofactor of A[i][j] 
-            getCofactor(A, temp, i, j, N); 
-  
-            // sign of adj[j][i] positive if sum of row 
-            // and column indexes is even. 
-            sign = ((i+j)%2==0)? 1: -1; 
-  
-            // Interchanging rows and columns to get the 
-            // transpose of the cofactor matrix 
-            adj[j][i] = (sign)*(determinant(temp, N-1)); 
-        } 
-    } 
-} 
 /*above function perform adjoint opertion */
-	
-
-
 
 void adjoint(int A[N][N],int adj[N][N]) 
 { 
